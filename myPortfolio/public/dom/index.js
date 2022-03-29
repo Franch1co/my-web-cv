603c692cbf500d74b1c0f95e08099bd361cea77e
Francisco Gouveia <fran_gou@hotmail.com>
2022-03-29 22:53:55 +0100

    My webCV is almost done! ohh yeah

--- myPortfolio/public/dom/index.js
+++ myPortfolio/public/dom/index.js
@@ -17,29 +17,88 @@ nav.addEventListener('click', (e) => {
     navbarNav.style.backgroundColor = '#bbb5ae';
 })
 
-const textDisplay = document.querySelector('#textDisplay');
+const writerDisplay = () => {
 
-var typewriter = new Typewriter(textDisplay, {
-  loop: true,
-  delay: 75,
-});
+  const textDisplay = document.querySelector('#textDisplay');
 
-typewriter
-  .pauseFor(2500)
-  .typeString("Hi, I'm Francisco and I'm a programmer.")
-  .pauseFor(300)
-  .deleteChars(17)
-  .typeString('I love to develop new things!')
-  .pauseFor(300)
-  .deleteChars(51)
-  .typeString('I love new challenges!')
-  .pauseFor(400)
-  .deleteChars(15)
-  .typeString('to learn new technologies!')
-  .pauseFor(1000)
-  .start();
+  let typewriter = new Typewriter(textDisplay, {
+    loop: true,
+    delay: 75,
+  });
 
-const cursor = document.querySelector('.Typewriter__cursor');
-cursor.classList.remove('Typewriter__cursor');
+  typewriter
+    .pauseFor(2500)
+    .typeString("Hi, I'm Francisco and I'm a programmer.")
+    .pauseFor(300)
+    .deleteChars(17)
+    .typeString('I love to develop new things!')
+    .pauseFor(300)
+    .deleteChars(51)
+    .typeString('I love new challenges!')
+    .pauseFor(400)
+    .deleteChars(15)
+    .typeString('to learn new technologies!')
+    .pauseFor(1000)
+    .start();
+
+  // const cursor = document.querySelector('.Typewriter__cursor');
+  // cursor.classList.remove('Typewriter__cursor');
+
+}
+
+writerDisplay();
+
+const ENs = document.querySelectorAll('#en');
+const PTs = document.querySelectorAll('#pt');
+const EN = document.querySelector('#enMyText');
+
+function changePortuguese() {
+
+  EN.innertext = '';
+
+  const textDisplay = document.querySelector('#textDisplay');
+
+  let typewriter = new Typewriter(textDisplay, {
+    loop: true,
+    delay: 75,
+  });
+
+  typewriter
+    .pauseFor(2500)
+    .typeString("Olá, sou o Francisco e sou um programador.")
+    .pauseFor(300)
+    .deleteChars(19)
+    .typeString('adoro desenvolver novas coisas!')
+    .pauseFor(300)
+    .deleteChars(54)
+    .typeString('Eu adoro novos desafios!')
+    .pauseFor(400)
+    .deleteChars(15)
+    .typeString('aprender novas tecnologias!')
+    .pauseFor(1000)
+    .start();
+
+  for (let EN of ENs){
+    EN.classList.add('visually-hidden');
+  }
+  for (let PT of PTs){
+    PT.classList.remove('visually-hidden');
+  }
+}
+
+function changeEnglish() {
+
+  writerDisplay();
+
+  for (let PT of PTs){
+    PT.classList.add('visually-hidden');
+  }
+
+  for (let EN of ENs){
+    EN.classList.remove('visually-hidden');
+  }  
+}
+
+
 
 
